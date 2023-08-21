b0f12441168370a8034a529e776f4573c20c041e
Clarence Jordan <clarencejordan087@gmail.com>
2023-08-21 10:10:17 -0500

    fiexed the aurther name so now you know who the quote is from

--- src/components/QuotesAPI.js
+++ src/components/QuotesAPI.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function QuotesAPI() {
   const [quote, setQuote] = useState("loading...");
+  const [aurther,setqurther] = useState('')
   const apiKey = "tM06yAPvOhKRwN5vmKmC4Q==KUwURLAqAADV8j4c";
   const url = "https://api.api-ninjas.com/v1/quotes?category=inspirational";
 
@@ -14,11 +15,11 @@ export default function QuotesAPI() {
           },
         });
         const data = await response.json();
+        console.log()
+        if (data) {
 
-        if (data && data.length > 0) {
-          const randomIndex = Math.floor(Math.random() * data.length);
-          const randomQuote = data[randomIndex].quote;
-          setQuote(randomQuote);
+          setQuote(data[0].quote);
+          setqurther(data[0].author);
         } else {
           setQuote("No quotes available");
         }
@@ -31,11 +32,12 @@ export default function QuotesAPI() {
     fetchQuote();
   }, []);
 
-  console.log(quote);
+
 
   return (
     <>
       <p>{quote}</p>
+      <p>-{aurther}</p>
     </>
   );
 }
